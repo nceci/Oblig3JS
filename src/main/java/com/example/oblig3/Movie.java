dd8114b559edd2f7ba2f07a7ce59354c5471449b
nceci <cengu2389@oslomet.no>
2024-04-22 13:47:21 +0200

    POJO

---
+++ src/main/java/com/example/oblig3/Movie.java
@@ -0,0 +1,18 @@
+package com.example.oblig3;
+
+public class Movie {
+    private String movie;
+
+    public Movie(String movie) {
+        this.movie = movie;
+    }
+    public Movie(){}
+
+    public String getMovie() {
+        return movie;
+    }
+
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+}
