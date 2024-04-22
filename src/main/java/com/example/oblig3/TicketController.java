6f83b17713698381274f248d8dec52c7a1a58cc0
nceci <cengu2389@oslomet.no>
2024-04-22 14:03:37 +0200

    POJO

--- src/main/java/com/example/oblig3/TicketController.java
+++ src/main/java/com/example/oblig3/TicketController.java
@@ -1,4 +1,27 @@
 package com.example.oblig3;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class TicketController {
+ private final List<Ticket> tickets = new ArrayList<>();
+
+ @PostMapping("/saveTicket")
+    public void saveTicket(Ticket inTicket){
+     tickets.add(inTicket);
+ }
+ @GetMapping("/getMovie")
+    public List<Movie> movies(){
+     List<Movie> newMovie = new ArrayList<>();
+     newMovie.add(new Movie("Back to Black"));
+     newMovie.add(new Movie("One life"));
+     newMovie.add(new Movie("Ibelin"));
+     return newMovie;
+ }
+
 }
